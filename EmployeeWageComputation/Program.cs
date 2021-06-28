16879602b2d162accccac4722d0745aed5137923
AkshayKumarTN <tnak369@gmail.com>
2021-06-28 17:42:10 +0530

    [Akshay] UseCase1

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -6,7 +6,17 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int employeePresent = 1;
+            Random random = new Random();
+            int empInput = random.Next(0,2);
+            if(empInput==employeePresent)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
