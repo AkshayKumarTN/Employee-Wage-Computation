d750715d9af69d42f9f641e4641b508c5c260062
AkshayKumarTN <tnak369@gmail.com>
2021-07-04 12:40:00 +0530

    [Akshay] Added -> Save Total Wage By Company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,8 +7,10 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("\n Employee Wage Computation \n");
-            EmployeeWage.computeEmpWage("DELL",20,20,100);
-            EmployeeWage.computeEmpWage("HP", 20, 20, 100);
+            EmployeeWage dell=new EmployeeWage ("DELL",20,20,100);
+            EmployeeWage hp=new EmployeeWage ("HP", 20, 20, 100);
+            dell.computeEmpWage();
+            hp.computeEmpWage();
         }
     }
 }
